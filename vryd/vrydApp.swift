dfe30ffbff44c8d0f021fb6caaa4ae6c7f032278
qkeele <quincykeele@gmail.com>
2026-02-11 08:59:06 -1000

    Initial Commit

---
+++ vryd/vrydApp.swift
@@ -0,0 +1,17 @@
+//
+//  vrydApp.swift
+//  vryd
+//
+//  Created by Stratton Keele on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct vrydApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
